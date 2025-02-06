8b5a2b4497614fbd7b77b661a9e260fb95a6878a
dlgksqls <horainlee87@gmail.com>
2025-02-06 17:47:24 +0900

    study

---
+++ DOIT/자료구조/슬라이딩윈도우/boj11003_나중에_풀어보기.java
@@ -0,0 +1,8 @@
+package 자료구조.슬라이딩윈도우;
+
+public class boj11003_나중에_풀어보기 {
+
+    public static void main(String[] args){
+
+    }
+}
--- DOIT/자료구조/슬라이딩윈도우/boj12891.java
+++ DOIT/자료구조/슬라이딩윈도우/boj12891.java
@@ -12,60 +12,115 @@ public class boj12891 {
      * 두번 째 줄에는 민호가 임의로 만든 DNA 문자열이 주어진다.
      * 세번 째 줄에는 부분문자열에 포함되어야 할 {‘A’, ‘C’, ‘G’, ‘T’} 의 최소 개수가 공백을 구분으로 주어진다.
      * 각각의 수는 |S| 보다 작거나 같은 음이 아닌 정수이며 총 합은 |S| 보다 작거나 같음이 보장된다.
+     *
+     * 입력
+     * 9 8
+     * CCTGGATTG
+     * 2 0 1 1
      */
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+    static int[] dna = new int[4];
+    static int[] check_dna = new int[4];
+    static int checkSecret = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int s = Integer.parseInt(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
-
-        String dna = br.readLine();
-
-        st = new StringTokenizer(br.readLine());
-
-        int a = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int g = Integer.parseInt(st.nextToken());
-        int t = Integer.parseInt(st.nextToken());
-
-        int a_count = 0;
-        int c_count = 0;
-        int g_count = 0;
-        int t_count = 0;
-
-        int start_index = 0;
-        int end_index = p-1;
         int answer = 0;
 
-        while (end_index < dna.length()){
-            String substring = dna.substring(start_index, end_index+1);
-            for (int i=0; i<substring.length(); i++) {
-                if (substring.charAt(i) == 'A'){
-                    a_count ++;
-                }
-                else if(substring.charAt(i) == 'C'){
-                    c_count ++;
-                }
-                else if(substring.charAt(i) == 'G'){
-                    g_count ++;
-                }
-                else if(substring.charAt(i) == 'T'){
-                    t_count ++;
-                }
-            }
-            start_index ++;
-            end_index ++;
+        String dna_sen = br.readLine();
 
-            if (a_count == a && c_count == c && g_count == g && t_count == t){
-                answer += 1;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<4; i++){
+            dna[i] = Integer.parseInt(st.nextToken());
+            if (dna[i] == check_dna[i]){
+                checkSecret ++;
             }
+        }
+        
+        for (int i=0; i<p; i++){
+            add_check(dna_sen.charAt(i));
+        }
 
-            a_count = 0;
-            c_count = 0;
-            g_count = 0;
-            t_count = 0;
+        if (checkSecret == 4){
+            answer ++;
+        }
+
+        for (int i=p; i<s; i++){
+            int start_index = i - p;
+            int end_index = i;
+
+            add_check(dna_sen.charAt(end_index));
+            remove_check(dna_sen.charAt(start_index));
+
+            if (checkSecret == 4){
+                answer ++;
+            }
         }
         System.out.println(answer);
+        br.close();
+    }
+
+    private static void add_check(char c) {
+        switch (c){
+            case 'A':
+                check_dna[0] ++;
+                if (dna[0] == check_dna[0]){
+                    checkSecret ++;
+                }
+                break;
+            case 'C':
+                check_dna[1] ++;
+                if (dna[1] == check_dna[1]){
+                    checkSecret ++;
+                }
+                break;
+            case 'G':
+                check_dna[2] ++;
+                if (dna[2] == check_dna[2]){
+                    checkSecret ++;
+                }
+                break;
+            case 'T':
+                check_dna[3] ++;
+                if (dna[3] == check_dna[3]){
+                    checkSecret ++;
+                }
+                break;
+        }
+    }
+
+    private static void remove_check(char c) {
+        switch (c){
+            case 'A':
+                if (dna[0] == check_dna[0]){
+                    checkSecret --;
+                }
+                check_dna[0] --;
+                break;
+            case 'C':
+                if (dna[1] == check_dna[1]){
+                    checkSecret --;
+                }
+                check_dna[1] --;
+                break;
+            case 'G':
+                if (dna[2] == check_dna[2]){
+                    checkSecret --;
+                }
+                check_dna[2] --;
+
+                break;
+            case 'T':
+                if (dna[3] == check_dna[3]){
+                    checkSecret --;
+                }
+                check_dna[3] --;
+                break;
+        }
     }
 }
