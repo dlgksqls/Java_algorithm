0c0f681891bf3308d775346d2b0d0a8d43f9c96d
dlgksqls <horainlee87@gmail.com>
2025-02-21 13:53:41 +0900

    study

---
+++ DOIT/자료구조/정렬/boj1427.java
@@ -0,0 +1,45 @@
+package 자료구조.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class boj1427 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine();
+
+        char[] array_char = n.toCharArray();
+        Integer[] array_int = new Integer[array_char.length];
+
+        for(int i=0; i<array_char.length; i++) {
+            array_int[i] = Integer.parseInt(String.valueOf(array_char[i]));
+        }
+
+//        Arrays.sort(array_int, Comparator.reverseOrder());
+//
+//        for(int i=0; i<array_int.length; i++){
+//            System.out.print(array_int[i]);
+//        }
+
+        for (int i=0; i<array_int.length; i++){
+            int max = i;
+            for (int j=i+1; j<array_int.length; j++){
+                if (array_int[max] < array_int[j]){
+                    max = j;
+                }
+            }
+            if (array_int[i] < array_int[max]){
+                int temp = array_int[max];
+                array_int[max] = array_int[i];
+                array_int[i] = temp;
+            }
+            System.out.print(array_int[i]);
+        }
+    }
+}
---
+++ DOIT/자료구조/정렬/boj2750.java
@@ -0,0 +1,32 @@
+package 자료구조.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+
+        for(int i=0; i<n; i++){
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i=0; i<n-1; i++){
+            for (int j=0; j<n-1; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i=0; i<n; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
