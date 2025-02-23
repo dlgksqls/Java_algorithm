91cc9e68682139bff04f95b1d1ebc969b18f47c6
dlgksqls <horainlee87@gmail.com>
2025-02-24 07:13:47 +0900

    탐색 하기

---
+++ DOIT/자료구조/탐색/boj11724.java
@@ -0,0 +1,59 @@
+package 자료구조.탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class boj11724 {
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+
+        for(int i=0; i<n+1; i++){
+            graph[i] = new ArrayList<Integer>();
+        }
+
+        for (int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        int count = 0;
+        for (int i=1; i<n+1; i++){
+            if (!visited[i]){
+                count ++;
+                dfs(i);
+            }
+        }
+        System.out.println(count);
+        br.close();
+    }
+
+    private static void dfs(int v) {
+        if (visited[v]){
+            return;
+        }
+        visited[v] = true;
+        for (Integer i : graph[v]) {
+            if (!visited[i]){
+                dfs(i);
+            }
+        }
+    }
+}
---
+++ DOIT/자료구조/탐색/boj2023_문제_이해하기.java
@@ -0,0 +1,49 @@
+package 자료구조.탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2023_문제_이해하기 {
+
+    static int n;
+
+    // 맨 상위 숫자는 2, 3, 5, 7
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+
+        br.close();
+    }
+
+    private static void dfs(int number, int digits) {
+        if (digits == n){
+            if (isPrime(number)){
+                System.out.println(number);
+            }
+            return;
+        }
+        for(int i=1; i<10; i++){
+            if (i%2==0){
+                continue;
+            }
+            if (isPrime(number * 10 + i)){
+                dfs(number * 10 + i, digits + 1);
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i=2; i<=number/2; i++){
+            if (number%i == 0)
+                return false;
+        }
+        return true;
+    }
+}
