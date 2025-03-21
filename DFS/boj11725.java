b3308d4c8243bf57bf428ac2e0d87b49f888e35c
dlgksqls <horainlee87@gmail.com>
2025-03-21 20:48:38 +0900

    study

--- DFS/boj11725.java
+++ DFS/boj11725.java
@@ -1,48 +1,51 @@
 import java.util.*;
 import java.io.*;
 public class boj11725 {
-    static int max = 100000+10; // 10만개 정도 들어오면 ArrayList 사용
+
     static int n;
     static ArrayList<Integer>[] graph;
-    static boolean visited[];
-    static int answer[];
+    static int[] visited;
+    static int[] result;
 
-    public static void dfs(int index){
-        visited[index] = true;
-
-        for (int i=0; i<graph[index].size(); i++){
-            int next = graph[index].get(i);
-            if (visited[next] == false) {
-                answer[next] = index;
-                dfs(next);
-            }
-        }
-    }
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
-        graph = new ArrayList[max];
+        graph = new ArrayList[n+1];
+        visited = new int[n+1];
+        result = new int[n+1];
 
         for (int i=1; i<=n; i++){
             graph[i] = new ArrayList<>();
         }
-        visited = new boolean[max];
-        answer = new int[max];
 
         for (int i=0; i<n-1; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            graph[x].add(y);
-            graph[y].add(x);
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        dfs(1);
+        for(int i=1; i<=n; i++){
+            if (visited[i] != 1) {
+                dfs(i);
+            }
+        }
+        for(int i=2; i<=n; i++){
+            System.out.println(result[i]);
+        }
+    }
 
-        for (int i=2; i<=n; i++)
-            System.out.println(answer[i]);
+    private static void dfs(int v) {
+        visited[v] = 1;
+
+        for (int i : graph[v]){
+            if (visited[i] != 1) {
+                result[i] = v;
+                dfs(i);
+            }
+        }
     }
 }
---
+++ DOIT/그래프/복습/boj1325.java
@@ -0,0 +1,77 @@
+package 그래프.복습;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj1325 {
+    static int n;
+    static int m;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] result;
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        result = new int[n+1];
+
+        for (int i=1; i<=n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[b].add(a);
+        }
+
+        for(int i=1; i<=n; i++){
+            count = 0;
+            visited = new boolean[n+1];
+            bfs(i);
+            result[i] = count;
+        }
+
+        int max = 0;
+        for (int i : result){
+            if (max < i) max = i;
+        }
+
+        for (int i=1; i<=n; i++){
+            if (result[i] == max) System.out.println(i);
+        }
+
+        br.close();
+    }
+
+    private static void bfs(int i) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(i);
+
+        visited[i] = true;
+
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            for (int v : graph[now]){
+                if (!visited[v]){
+                    visited[v] = true;
+                    queue.add(v);
+                    count ++;
+                }
+            }
+        }
+    }
+}
