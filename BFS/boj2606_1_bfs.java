044cb8818cf8b62cad3b8b5b71d5dc645e4369bc
dlgksqls <horainlee87@gmail.com>
2025-04-16 14:19:40 +0900

    study

---
+++ BFS/boj2606_1_bfs.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj2606_1_bfs {
+    static ArrayList<Integer>[] graph;
+    static boolean visited[];
+    static int n, m;
+    static int answer = 0;
+    static Queue<Integer> queue;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for(int i=1; i<=n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i=0; i<m; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+
+        bfs(1);
+        System.out.println(answer);
+
+        br.close();
+    }
+
+    private static void bfs(int node) {
+        queue = new LinkedList<>();
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()){
+            int v = queue.poll();
+            for (Integer i: graph[v]) {
+                if (!visited[i]){
+                    visited[i] = true;
+                    answer ++;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
+}
---
+++ DFS/boj2606_1.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class boj2606_1 {
+
+    static int n;
+    static int m;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for(int i=1; i<=n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        recursion(1);
+        System.out.println(answer);
+
+        br.close();
+    }
+
+    private static void recursion(int node) {
+        visited[node] = true;
+
+        for (int v : graph[node]) {
+            if (!visited[v]) {
+                answer ++;
+                recursion(v);
+            }
+        }
+    }
+}
