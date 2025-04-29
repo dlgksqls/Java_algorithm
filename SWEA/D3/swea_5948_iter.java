30d65c9ccc9a0ecf620925a5adf77c1640562378
dlgksqls <horainlee87@gmail.com>
2025-04-29 14:59:41 +0900

    study

---
+++ SWEA/D3/swea_5948_iter.java
@@ -0,0 +1,36 @@
+package D3;
+
+import java.util.*;
+
+public class swea_5948_iter {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            Integer[] array = new Integer[7];
+            TreeSet<Integer> hashSet = new TreeSet<>(Collections.reverseOrder());
+
+            for(int i=0; i<7; i++){
+                array[i] = sc.nextInt();
+            }
+
+            Arrays.sort(array, Collections.reverseOrder());
+            for(int i=0; i<5; i++){
+                for(int j=i+1; j<6; j++){
+                    for (int k=j+1; k<7; k++){
+                        hashSet.add(array[i] + array[j] + array[k]);
+                    }
+                }
+            }
+
+            int count = 1;
+            for (Integer i : hashSet) {
+                if (count == 5)
+                    System.out.println("#" + tc + " " + i);
+                count ++;
+            }
+        }
+    }
+}
