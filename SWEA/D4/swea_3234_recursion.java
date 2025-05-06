1d6e2b86ddc2ee7ea6f5c60b13265c0e3777ff58
dlgksqls <horainlee87@gmail.com>
2025-05-06 14:07:05 +0900

    study

---
+++ SWEA/D4/swea_3234_recursion.java
@@ -0,0 +1,71 @@
+package D4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_3234_recursion {
+    static int answer;
+    static int n;
+    static int[] array;
+    static int[] per;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <= T; tc++) {
+            n = Integer.parseInt(br.readLine());
+            array = new int[n];
+            visited = new boolean[n];
+            per = new int[n];
+
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < n; i++) {
+                array[i] = Integer.parseInt(st.nextToken());
+            }
+
+            answer = 0;
+
+            combination(0);
+
+            System.out.println("#" + tc + " " + answer);
+        }
+        br.close();
+    }
+
+    private static void combination(int depth) {
+        if (depth == n){
+            recursion(0, 0, 0);
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if (!visited[i]){
+                visited[i] = true;
+                per[depth] = array[i];
+                combination(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static void recursion(int idx, int left, int right) {
+        if (left < right) {
+            return;
+        }
+
+        if (idx == n){
+            answer ++;
+            return;
+        }
+
+        recursion(idx + 1, left + per[idx], right);
+
+        recursion(idx + 1, left, right + per[idx]);
+    }
+}
