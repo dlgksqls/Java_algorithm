17f18a8c51293187b42fde9b37fbaf3334bd7919
dlgksqls <horainlee87@gmail.com>
2025-05-07 14:05:19 +0900

    study

---
+++ SWEA/D4/swea_1219.java
@@ -0,0 +1,81 @@
+package D4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class swea_1219 {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static boolean flag;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for(int tc = 1; tc <= 10; tc ++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            graph = new ArrayList[100];
+            for(int i=0; i<100; i++){
+                graph[i] = new ArrayList<>();
+            }
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<m; i++){
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                graph[x].add(y);
+            }
+
+            flag = false;
+            visited = new boolean[100];
+//            dfs(0);
+            bfs(0);
+
+            if (flag) System.out.println("#" + tc + " " + 1);
+            else System.out.println("#" + tc + " " + 0);
+        }
+
+        br.close();
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[v] = true;
+        queue.add(v);
+
+        while(!queue.isEmpty()){
+            Integer now = queue.poll();
+
+            for (Integer i : graph[now]) {
+                if (i == 99){
+                    flag = true;
+                    return;
+                }
+                if (!visited[i]){
+                    queue.add(i);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int v) {
+        if (v == 99){
+            flag = true;
+            return;
+        }
+        visited[v] = true;
+
+        for (Integer i : graph[v]) {
+            if (!visited[i]){
+                dfs(i);
+            }
+        }
+    }
+}
---
+++ SWEA/D4/swea_5643.java
@@ -0,0 +1,66 @@
+package D4;
+
+import java.io.*;
+import java.util.*;
+
+public class swea_5643 {
+    static int N;
+    static ArrayList<Integer>[] taller;
+    static ArrayList<Integer>[] shorter;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            int M = Integer.parseInt(br.readLine());
+
+            taller = new ArrayList[N+1];
+            shorter = new ArrayList[N+1];
+
+            for(int i = 1; i <= N; i++) {
+                taller[i] = new ArrayList<>();
+                shorter[i] = new ArrayList<>();
+            }
+
+            for(int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                taller[a].add(b);   // a보다 큰 학생 b
+                shorter[b].add(a);  // b보다 작은 학생 a
+            }
+
+            int answer = 0;
+            for(int i = 1; i <= N; i++) {
+                boolean[] visited = new boolean[N+1];
+                int tallCount = dfs(i, taller, visited);
+
+                visited = new boolean[N+1];
+                int shortCount = dfs(i, shorter, visited);
+
+                if (tallCount + shortCount == N - 1) {
+                    answer++;
+                }
+            }
+
+            System.out.println("#" + tc + " " + answer);
+        }
+    }
+
+    private static int dfs(int cur, ArrayList<Integer>[] graph, boolean[] visited) {
+        visited[cur] = true;
+        int count = 0;
+
+        for (int next : graph[cur]) {
+            if (!visited[next]) {
+                count += 1 + dfs(next, graph, visited);
+            }
+        }
+
+        return count;
+    }
+}
