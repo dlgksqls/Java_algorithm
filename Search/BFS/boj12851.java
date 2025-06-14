770035cc0c3de6298ac39354dfd518d43c073dc0
dlgksqls <horainlee87@gmail.com>
2025-06-14 14:45:36 +0900

    study

--- Search/BFS/boj12851.java
+++ Search/BFS/boj12851.java
@@ -8,38 +8,40 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj12851 {
+    static int[] time;
+    static int[] count;
     static final int MAX = 100001;
-    static int[] time = new int[MAX];
-    static int[] count = new int[MAX];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        bfs(n, k);
+        time = new int[MAX];
+        count = new int[MAX];
+
+        bfs(n);
 
         System.out.println(time[k]);
         System.out.println(count[k]);
+
         br.close();
     }
 
-    private static void bfs(int n, int k) {
+    private static void bfs(int n) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(n);
-
         time[n] = 0;
         count[n] = 1;
 
         while(!queue.isEmpty()){
             int now = queue.poll();
-
             int[] nextMoves = {now + 1, now - 1, now * 2};
             for (int nextMove : nextMoves) {
                 if (nextMove >= 0 && nextMove < MAX){
-                    if (time[nextMove] == 0 && nextMove != n){
+                    if (time[nextMove] == 0 && nextMove != n) {
                         time[nextMove] = time[now] + 1;
                         count[nextMove] = count[now];
                         queue.add(nextMove);
