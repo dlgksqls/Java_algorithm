0030022ee9acaf70900270c110494e5bc9a7c66e
dlgksqls <horainlee87@gmail.com>
2025-07-01 15:54:28 +0900

    study

--- BinarySearch/boj2805.java
+++ BinarySearch/boj2805.java
@@ -5,40 +5,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class boj2805 {
-
-    static int n;
-    static long m;
-    static int[] array;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Long.parseLong(st.nextToken());
 
-        array = new int[n];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] array = new int[n];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        int start = 1;
+//        Arrays.sort(array);
+
+        int start = 0;
         int end = Arrays.stream(array).max().getAsInt();
 
         while(start <= end){
             int mid = (start + end) / 2;
 
-            // 얼마나 나무를 채취했나?
             long wood = 0;
 
-            for (int tree : array) {
-                if (tree >= mid){
-                    wood += tree - mid;
+            for (int i : array) {
+                if (i >= mid){
+                    wood += i - mid;
                 }
             }
 
-            // 채취한 나무가 업인가 다운인가?
-            if (wood >= m) start = mid + 1; // 최소 만족하면 더 줄여보기
+            if (wood >= m) start = mid + 1;
             else end = mid - 1;
         }
 
